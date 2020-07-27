438d8daf02fa22eacdc457dfbdb85c8b494a3dfe
romele-stefano <romele.stefano@gmail.com>
2020-07-27 18:53:50 +0200

    #10 - Dynamic CSS classes

--- app.js
+++ app.js
@@ -1,24 +1,15 @@
 new Vue({
   el:'#vue-app',
   data: {   
-    age: 20,
-    a: 0,
-    b: 0
+    available: false,
+    nearby: false
   },
-  /* methods: {
-    addToA: function(){
-      return this.a + this.age
-    },
-    addToB: function(){
-      return this.b + this.age
-    }
-  }, */
   computed: {
-    addToA: function(){
-      return this.a + this.age
-    },
-    addToB: function(){
-      return this.b + this.age
+    compClasses: function(){
+      return {
+        available: this.available,
+        nearby: this.nearby
+      }
     }
   }
 })
