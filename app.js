755e04eb300b6a3fc3fe759053f9867126d13bcd
romele-stefano <romele.stefano@gmail.com>
2020-07-27 13:00:17 +0200

    #7 - Keyboard events

--- app.js
+++ app.js
@@ -1,25 +1,13 @@
 new Vue({
   el:'#vue-app',
-  data: {
-    age: 29,
-    x: 0,
-    y: 0
+  data: {   
   },
   methods: {
-    add: function(inc){
-      this.age += inc
+    logName: function(){
+      console.log('You entered your name')
     },
-    subtract: function(dec){
-      this.age -= dec
-    },
-    // we automatically get event when there is an event, we do not need to pass it into html
-    updateXY: function(event){
-      // console.log(event)
-      this.x = event.offsetX
-      this.y = event.offsetY
-    },
-    click: function(){
-      alert('You clicked me')
+    logAge: function(){
+      console.log('You entered your age')
     }
   }
 })
