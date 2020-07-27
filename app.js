c770834de2a9ebdeb02cbc691aa06a7d6379b9d2
romele-stefano <romele.stefano@gmail.com>
2020-07-27 08:32:45 +0200

    #4 - Data binding

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Vue({
   el:'#vue-app',
   data: {
     name: 'Stefano',
-    job: 'Full stack Dev'
+    job: 'Full stack Dev',
+    website: 'https://github.com/romele-stefano',
+    websiteTag: '<a href="https://github.com/romele-stefano">Another way to show Github link</a>'
   },
   methods: {
     greet: function(time){
