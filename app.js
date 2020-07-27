bac4adba08e8d03ae653710e309d168bbe6a5a8f
romele-stefano <romele.stefano@gmail.com>
2020-07-27 19:18:52 +0200

    #12 - Looping with v-for

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 new Vue({
   el:'#vue-app',
   data: {   
-    error: false,
-    success: false
+    name: 'Jack',
+    characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
+    ninjas: [
+      { name: 'Ryu', age: 25 },
+      { name: 'Yoshi', age: 35 },
+      { name: 'Ken', age: 55 }
+    ]
   }
 })
