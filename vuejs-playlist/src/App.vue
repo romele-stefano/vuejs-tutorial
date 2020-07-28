22b448e9827f586345d0674ecb2ecda485a50605
romele-stefano <romele.stefano@gmail.com>
2020-07-28 14:15:46 +0200

    #21 - Nesting components examples

--- vuejs-playlist/src/App.vue
+++ vuejs-playlist/src/App.vue
@@ -1,27 +1,32 @@
 <template>
-  <div>
-    <h1>{{ title }}</h1>
-    <ninjas></ninjas>
-  </div>
+    <div>
+        <app-header></app-header>
+        <app-ninjas></app-ninjas>
+        <app-footer></app-footer>
+    </div>
 </template>
 
 <script>
-import Ninjas from './Ninjas.vue'
-
+// Imports
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import Ninjas from './components/Ninjas.vue';
 export default {
-  data () {
-    return {
-      title: 'Ninja app'
+    components: {
+        'app-header': Header,
+        'app-footer': Footer,
+        'app-ninjas': Ninjas
+    },
+    data () {
+        return {
+        }
     }
-  },
-  components: {
-    'ninjas': Ninjas
-  }
 }
 </script>
 
 <style>
-h1 {
-  color: purple;
+body{
+    margin: 0;
+    font-family: 'Nunito SemiBold';
 }
 </style>
