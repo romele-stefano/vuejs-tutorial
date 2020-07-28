c15eb922fa98e1fef5876e476560f2f425755c75
romele-stefano <romele.stefano@gmail.com>
2020-07-28 07:57:22 +0200

    #15 - Intro to components

--- app.js
+++ app.js
@@ -1,31 +1,22 @@
-var one = new Vue({
-  el:'#vue-app-one',
-  data: { 
-    title: 'Vue App One'  
+Vue.component('greeting', {
+  template: '<p>Hey there, I am {{ name }}. <button v-on:click="changeName">Change Name</button></p>',
+  data: function(){
+    return {
+      name: 'Yoshi'
+    }
   },
-  computed: {
-    greet: function(){
-      return 'Hello from app one'
+  methods: {
+    changeName: function(){
+      this.name = 'Mario'
     }
   }
+})
+
+var one = new Vue({
+  el:'#vue-app-one'
 })
 
 var two = new Vue({
-  el:'#vue-app-two',
-  data: {
-    title: 'Vue App Two'
-  },
-  methods: {
-    changeTitle: function(){
-      one.title = 'Title changed'
-    }
-  },
-  computed: {
-    greet: function(){
-      return 'Hello from app two'
-    }
-  }
+  el:'#vue-app-two'
 })
 
-// will run automatically
-two.title = 'Changed from outside Vue instance'
