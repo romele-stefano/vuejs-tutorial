506e6e7c97d46f1f67b8bb50331d16269b3530bb
romele-stefano <romele.stefano@gmail.com>
2020-08-01 13:34:33 +0200

    #41 - Adding router links

--- vuejs-playlist/src/routes.js
+++ vuejs-playlist/src/routes.js
@@ -3,6 +3,8 @@ import showBlogs from './components/showBlogs.vue'
 import addBlog from './components/addBlog.vue'
 
 export default [
-  {path: '/', component: showBlogs},
-  {path: '/add', component: addBlog}
+  { path: '/', component: showBlogs },
+  { path: '/add', component: addBlog },
+  // if path does not exist
+  { path: '*', redirect: '/'}
 ]
